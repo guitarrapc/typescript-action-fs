401a0f21c9aacb7c0c9de00411d5e6fa2badfb57
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-06-28 18:29:00 +0900

    chore: split test file

---
+++ __tests__/io.test.ts
@@ -0,0 +1,44 @@
+import * as path from 'path'
+import { expect, test } from '@jest/globals'
+import { mv, mkdirP } from '../src/io'
+import * as fs from 'fs'
+
+test('io/mv file runs', async () => {
+  const dir = path.join(__dirname, '..', 'tmp/io_mv/file')
+  const src = path.join(dir, 'foo')
+  const dest = path.join(dir, 'bar')
+
+  fs.mkdirSync(dir, { recursive: true })
+  fs.rmSync(dest, { force: true })
+  fs.writeFileSync(src, 'hello world', 'utf8')
+
+  await mv(src, dest)
+
+  expect(fs.existsSync(src)).toBe(false)
+  expect(fs.existsSync(dest)).toBe(true)
+})
+
+test('io/mv dir runs', async () => {
+  const dir = path.join(__dirname, '..', 'tmp/io_mv/dir')
+  const src = path.join(dir, 'foo')
+  const dest = path.join(dir, 'bar')
+
+  fs.mkdirSync(src, { recursive: true })
+  fs.rmSync(dest, { recursive: true, force: true })
+
+  await mv(src, dest)
+
+  expect(fs.existsSync(src)).toBe(false)
+  expect(fs.existsSync(dest)).toBe(true)
+})
+
+test('io/mkdirP runs', async () => {
+  const dir = path.join(__dirname, '..', 'tmp/io_mkdirP')
+  const dirName = 'test-path'
+  const virtualWorkspacePath = path.join(dir, dirName)
+
+  fs.rmSync(virtualWorkspacePath, { recursive: true, force: true })
+
+  await mkdirP(virtualWorkspacePath)
+  expect(fs.existsSync(virtualWorkspacePath)).toBe(true)
+})
--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,52 +1,9 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
-import {mv, mkdirP} from '../src/io'
+import { expect, test } from '@jest/globals'
 import * as fs from 'fs'
-
-test('io/mv file runs', async () => {
-  const src = path.join(__dirname, '..', 'tmp/file', 'foo')
-  const dest = path.join(__dirname, '..', 'tmp/file', 'bar')
-
-  fs.mkdirSync(path.join(__dirname, '..', 'tmp/file'), {recursive: true})
-  fs.writeFileSync(src, 'hello world', 'utf8')
-  fs.rmSync(dest, {force: true})
-
-  await mv(src, dest)
-
-  expect(fs.existsSync(src)).toBe(false)
-  expect(fs.existsSync(dest)).toBe(true)
-})
-
-test('io/mv dir runs', async () => {
-  const src = path.join(__dirname, '..', 'tmp/io_mv/dir', 'foo')
-  const dest = path.join(__dirname, '..', 'tmp/io_mv/dir', 'bar')
-
-  fs.mkdirSync(src, {recursive: true})
-  fs.rmSync(dest, {recursive: true, force: true})
-
-  await mv(src, dest)
-
-  expect(fs.existsSync(src)).toBe(false)
-  expect(fs.existsSync(dest)).toBe(true)
-})
-
-test('io/mkdirP runs', async () => {
-  const virtualWorkspacePath = path.join(
-    __dirname,
-    '..',
-    'tmp/io_mkdirP',
-    'test-path'
-  )
-
-  await mkdirP(virtualWorkspacePath)
-  expect(fs.existsSync(virtualWorkspacePath)).toBe(true)
-
-  fs.rmSync(virtualWorkspacePath, {recursive: true, force: true})
-  expect(fs.existsSync(virtualWorkspacePath)).toBe(false)
-})
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -63,7 +20,7 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  const githubWorkspace = path.join(__dirname, '..', 'tmp/integrate')
+  const githubWorkspace = path.join(__dirname, '..', 'tmp/main')
   const testPath = 'test-path'
   const src = path.join(githubWorkspace, 'test.txt')
   process.env['INPUT_MILLISECONDS'] = '500'
@@ -71,8 +28,13 @@ test('test runs', () => {
   process.env['INPUT_WORKSPACE'] = 'test-path'
   process.env['GITHUB_WORKSPACE'] = githubWorkspace
 
-  fs.mkdirSync(githubWorkspace, {recursive: true})
+  fs.mkdirSync(githubWorkspace, { recursive: true })
   fs.writeFileSync(src, 'hello world', 'utf8')
+  fs.rmSync(`${src}.bak`, { force: true })
+  fs.rmSync(path.join(githubWorkspace, testPath), {
+    recursive: true,
+    force: true
+  })
 
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
@@ -80,10 +42,4 @@ test('test runs', () => {
     env: process.env
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
-
-  fs.rmSync(`${src}.bak`, {force: true})
-  fs.rmSync(path.join(githubWorkspace, testPath), {
-    recursive: true,
-    force: true
-  })
 })
