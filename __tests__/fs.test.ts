501fd61e7ecfe2484bca75c68702dd9799e81d09
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-06-28 19:13:34 +0900

    feat: add symlink empty test

--- __tests__/fs.test.ts
+++ __tests__/fs.test.ts
@@ -47,3 +47,26 @@ test('fs/symlink file runs', async () => {
   expect(fs.existsSync(link)).toBe(true)
   expect(fs.readFileSync(link, 'utf8')).toBe('hello world')
 })
+
+test('fs/symlink empty runs', async () => {
+  const dir = path.join(__dirname, '..', 'tmp/fs_symlink/empty')
+  const target = path.join(dir, 'original')
+  const link = path.join(dir, 'link')
+
+  fs.mkdirSync(dir, {recursive: true})
+  fs.writeFileSync(target, 'hello world', 'utf8')
+  if (fs.existsSync(link)) {
+    fs.unlinkSync(link)
+  }
+
+  expect(fs.existsSync(target)).toBe(true)
+  expect(fs.existsSync(link)).toBe(false)
+
+  await symlink(target, link) // no type specify makes file symlink on Windows.
+
+  expect(fs.existsSync(target)).toBe(true)
+  expect(fs.existsSync(link)).toBe(true)
+  expect(fs.readFileSync(link, 'utf8')).toBe('hello world')
+})
+
+// don't need junktion test.
